b596205aefac5e2bf2de913fa73d672bbfa1d99b
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-09 08:50:45 +0000

    Reset to 504e36a8b0d98da6e8bef519e99409fbf0383c17 and made changes for LLM Round 1

--- aioclock/ext/fast.py
+++ aioclock/ext/fast.py
@@ -24,53 +24,12 @@ except ImportError:
         "You need to install fastapi to use aioclock with FastAPI. Please run `pip install aioclock[fastapi]`"
     )
 
-
 def make_fastapi_router(aioclock: AioClock, router: Union[APIRouter, None] = None):
     """Make a FastAPI router that exposes the tasks of the AioClock instance and its external python API in HTTP Layer.
     You can pass a router to this function, and have dependencies injected in the router, or any authorization logic that you want to have.
 
     Example:
-        \"\"\"python
-        import asyncio
-        from contextlib import asynccontextmanager
-
-        from fastapi import FastAPI
-
-        from aioclock import AioClock
-        from aioclock.ext.fast import make_fastapi_router
-        from aioclock.triggers import Every, OnStartUp
-
-        clock_app = AioClock()
-
-        @clock_app.task(trigger=OnStartUp())
-        async def startup():
-            print("Starting...")
-
-        @clock_app.task(trigger=Every(seconds=3600))
-        async def foo():
-            print("Foo is processing...")
-
-
-        @asynccontextmanager
-        async def lifespan(app: FastAPI):
-            task = asyncio.create_task(clock_app.serve())
-            yield
-
-            try:
-                task.cancel()
-                await task
-            except asyncio.CancelledError:
-                ...
-
-
-        app = FastAPI(lifespan=lifespan)
-        app.include_router(make_fastapi_router(clock_app))
-
-        if __name__ == "__main__":
-            import uvicorn
-            # uvicorn.run(app)
-        \"\"\"
-    """
+"""
     router = router or APIRouter()
 
     @router.get("/tasks")
