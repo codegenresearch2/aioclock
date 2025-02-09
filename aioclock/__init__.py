51af71250f4b0393673be97b24c54ccd2cf97f46
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-09 08:50:46 +0000

    Reset to 27c3028b721465478ffba0018178f8dcd9019df7 and made changes for LLM Round 1

--- aioclock/__init__.py
+++ aioclock/__init__.py
@@ -2,18 +2,19 @@ from fast_depends import Depends
 
 from aioclock.app import AioClock
 from aioclock.group import Group
-from aioclock.triggers import At, Every, Forever, Once, OnShutDown, OnStartUp
+from aioclock.triggers import At, Every, Forever, Once, OnShutDown, OnStartUp, Cron
 
 __all__ = [
-    "Depends",
-    "Once",
-    "OnStartUp",
-    "OnShutDown",
-    "Every",
-    "Forever",
-    "Group",
-    "AioClock",
-    "At",
+    'Depends',
+    'Once',
+    'OnStartUp',
+    'OnShutDown',
+    'Every',
+    'Forever',
+    'Group',
+    'AioClock',
+    'At',
+    'Cron'
 ]
 
-__version__ = "0.1.1"
+__version__ = '0.1.1'
