b0e325972ebbbb68d41245e60af48e16cab6d4e4
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-09 17:04:22 +0000

    Reset to 504e36a8b0d98da6e8bef519e99409fbf0383c17 and made changes for LLM Round 1

--- aioclock/group.py
+++ aioclock/group.py
@@ -9,6 +9,7 @@ else:
     from typing import ParamSpec
 
 from fast_depends import inject
+from asyncer import asyncify
 
 from aioclock.provider import get_provider
 from aioclock.task import Task
@@ -19,7 +20,7 @@ P = ParamSpec("P")
 
 
 class Group:
-    def __init__(self, *, tasks: Union[list[Task], None] = None):
+    def __init__(self, *, tasks: Union[list[Task], None] = None, limiter=None):
         """
         Group of tasks that will be run together.
 
@@ -27,49 +28,39 @@ class Group:
         For example, you can have a group of tasks that are responsible for sending emails.
         And another group of tasks that are responsible for sending notifications.
 
-        Example:
-            
-
-            from aioclock import Group, Forever
-
-            email_group = Group()
-
-            # consider this as different file
-            @email_group.task(trigger=Forever())
-            def send_email():
-                ...
-
-            # app.py
-            aio_clock = AioClock()
-            aio_clock.include_group(email_group)
-            
-
+        Args:
+            tasks (list[Task], optional): List of tasks to initialize the group with.
+            limiter: Optional limiter object to limit the number of concurrent tasks.
         """
         self._tasks: list[Task] = tasks or []
+        self._limiter = limiter
 
     def task(self, *, trigger: BaseTrigger):
         """Function used to decorate tasks, to be registered inside AioClock.
 
-        Example:
-            
-            from aioclock import Group, Forever
-            @group.task(trigger=Forever())
-            def send_email():
-                ...
-            
+        The task decorator supports both coroutine and synchronous functions.
+        For synchronous functions, they are wrapped in a thread pool to ensure they do not block the event loop.
+
+        Args:
+            trigger (BaseTrigger): Trigger that will be used to run the function.
+
+        Returns:
+            Callable[[Callable[P, Union[Awaitable[T], T]]], Callable[P, Union[Awaitable[T], T]]]: Decorated function.
         """
 
-        def decorator(func: Callable[P, Awaitable[T]]) -> Callable[P, Awaitable[T]]:
+        def decorator(func: Callable[P, Union[Awaitable[T], T]]) -> Callable[P, Union[Awaitable[T], T]]:
             @wraps(func)
-            def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
-                return func(*args, **kwargs)
+            def wrapper(*args: P.args, **kwargs: P.kwargs) -> Union[Awaitable[T], T]:
+                if asyncio.iscoroutinefunction(func):
+                    return func(*args, **kwargs)
+                else:
+                    return asyncify(func)(*args, **kwargs)
 
-            self._tasks.append(
-                Task(
-                    func=inject(wrapper, dependency_overrides_provider=get_provider()),
-                    trigger=trigger,
-                )
+            task = Task(
+                func=inject(wrapper, dependency_overrides_provider=get_provider()),
+                trigger=trigger,
             )
+            self._tasks.append(task)
             return wrapper
 
         return decorator
@@ -79,7 +70,14 @@ class Group:
         Just for purpose of being able to run all task in group
         Private method, should not be used outside of the library
         """
-        await asyncio.gather(
-            *(task.run() for task in self._tasks),
-            return_exceptions=False,
-        )
+        if self._limiter:
+            async with self._limiter:
+                await asyncio.gather(
+                    *(task.run() for task in self._tasks),
+                    return_exceptions=False,
+                )
+        else:
+            await asyncio.gather(
+                *(task.run() for task in self._tasks),
+                return_exceptions=False,
+            )
