38dfa127701dbe547c1ea5462409fc3cbec1461e
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-09 06:51:11 +0000

    Reset to 504e36a8b0d98da6e8bef519e99409fbf0383c17 and made changes for LLM Round 1

--- aioclock/api.py
+++ aioclock/api.py
@@ -1,37 +1,14 @@
 from aioclock import AioClock, Once
-        from aioclock.api import run_specific_task
+from aioclock.api import run_specific_task
 
-        app = AioClock()
+app = AioClock()
 
-        @app.task(trigger=Once())
-        async def main():
-            print("Hello World")
+@app.task(trigger=Once())
+async def main():
+    """
+    This function prints 'Hello World' and returns a value.
+    """
+    print('Hello World')
 
-        async def some_other_func():
-            await run_specific_task(app._tasks[0].id, app)
-from aioclock import Once, AioClock, Depends
-        from aioclock.api import run_with_injected_deps
-
-        app = AioClock()
-
-        def some_dependency():
-            return 1
-
-        @app.task(trigger=Once())
-        async def main(bar: int = Depends(some_dependency)):
-            print("Hello World")
-            return bar
-
-        async def some_other_func():
-            foo = await run_with_injected_deps(main)
-            assert foo == 1
-from aioclock import AioClock, Once
-        from aioclock.api import get_metadata_of_all_tasks
-
-        app = AioClock()
-        @app.task(trigger=Once())
-        async def main():
-            pass
-
-        async def some_other_func():
-            metadata = await get_metadata_of_all_tasks(app)
+async def some_other_func():
+    await run_specific_task(app._tasks[0].id, app)
