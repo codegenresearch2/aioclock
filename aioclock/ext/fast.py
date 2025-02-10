1def959cd0e6f999bda4b0ddfb6fe9534fa2ffea
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-10 10:05:51 +0000

    Reset to 504e36a8b0d98da6e8bef519e99409fbf0383c17 and made changes for LLM Round 1

--- aioclock/ext/fast.py
+++ aioclock/ext/fast.py
@@ -3,16 +3,17 @@
 Use cases:
     - Expose the tasks of the AioClock instance in an HTTP API.
     - Show to your client which task is going to be run next, and at which time.
-    - Run a specific task from an HTTP API immidiately if needed.
+    - Run a specific task from an HTTP API immediately if needed.
 
 To use FastAPI Extension, please make sure you do `pip install aioclock[fastapi]`.
 
 """
 
-from typing import Union
+from typing import Union, Callable, Awaitable, TypeVar
 from uuid import UUID
+from asyncio import Semaphore
 
-from aioclock.api import TaskMetadata, get_metadata_of_all_tasks, run_specific_task
+from aioclock.api import TaskMetadata, get_metadata_of_all_tasks, run_specific_task, run_with_injected_deps
 from aioclock.app import AioClock
 from aioclock.exceptions import TaskIdNotFound
 
@@ -24,18 +25,19 @@ except ImportError:
         "You need to install fastapi to use aioclock with FastAPI. Please run `pip install aioclock[fastapi]`"
     )
 
+T = TypeVar("T")
 
-def make_fastapi_router(aioclock: AioClock, router: Union[APIRouter, None] = None):
+def make_fastapi_router(aioclock: AioClock, router: Union[APIRouter, None] = None, capacity: int = 10):
     """Make a FastAPI router that exposes the tasks of the AioClock instance and its external python API in HTTP Layer.
     You can pass a router to this function, and have dependencies injected in the router, or any authorization logic that you want to have.
 
-
-    params:
-        aioclock: AioClock instance to get the tasks from.
-        router: FastAPI router to add the routes to. If not provided, a new router will be created.
+    Attributes:
+        aioclock: AioClock instance to interact with.
+        router: Optional FastAPI router to use. If not provided, a new one will be created.
+        capacity: Maximum number of tasks that can be run concurrently.
 
     Example:
-        ```python
+        
         import asyncio
         from contextlib import asynccontextmanager
 
@@ -55,7 +57,6 @@ def make_fastapi_router(aioclock: AioClock, router: Union[APIRouter, None] = Non
         async def foo():
             print("Foo is processing...")
 
-
         @asynccontextmanager
         async def lifespan(app: FastAPI):
             task = asyncio.create_task(clock_app.serve())
@@ -67,16 +68,16 @@ def make_fastapi_router(aioclock: AioClock, router: Union[APIRouter, None] = Non
             except asyncio.CancelledError:
                 ...
 
-
         app = FastAPI(lifespan=lifespan)
-        app.include_router(make_fastapi_router(clock_app))
+        app.include_router(make_fastapi_router(clock_app, capacity=20))
 
         if __name__ == "__main__":
             import uvicorn
             # uvicorn.run(app)
-        ```
+        
     """
     router = router or APIRouter()
+    semaphore = Semaphore(capacity)
 
     @router.get("/tasks")
     async def get_tasks() -> list[TaskMetadata]:
@@ -84,9 +85,21 @@ def make_fastapi_router(aioclock: AioClock, router: Union[APIRouter, None] = Non
 
     @router.post("/task/{task_id}")
     async def run_task(task_id: UUID):
-        try:
-            await run_specific_task(task_id, aioclock)
-        except TaskIdNotFound:
-            raise HTTPException(status_code=404, detail="Task not found")
+        async with semaphore:
+            try:
+                await run_specific_task(task_id, aioclock)
+            except TaskIdNotFound:
+                raise HTTPException(status_code=404, detail="Task not found")
+
+    @router.post("/run/{task_name}")
+    async def run_task_by_name(task_name: str):
+        async with semaphore:
+            task = next((task for task in aioclock._tasks if task.func.__name__ == task_name), None)
+            if not task:
+                raise HTTPException(status_code=404, detail="Task not found")
+            await run_with_injected_deps(task.func)
 
     return router
+
+
+In the rewritten code, I have added a capacity limiter using a semaphore to limit the number of tasks that can be run concurrently. I have also added a new endpoint to run a task by its name. I have also clarified the functionality and parameters of the task decorator and added attributes for better understanding.
